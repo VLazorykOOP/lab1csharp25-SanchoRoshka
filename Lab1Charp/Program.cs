2e15fcb99f18d78dc04128f1534fa7e2e0746e8a
SanchoRoshka <159002573+SanchoRoshka@users.noreply.github.com>
2025-05-22 10:43:48 +0300

    Update Program.cs lab1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -14,3 +14,68 @@
 Console.WriteLine("Lab 1 !");
 task1();
 // continue ...
+
+class Task1
+{
+    static Random rand = new Random();
+
+    static int GeneratePoisson(double lambda)
+    {
+        int x = 0;
+        double P = Math.Exp(-lambda), sum = P, z = rand.NextDouble();
+        while (z > sum)
+        {
+            x++;
+            P *= lambda / x;
+            sum += P;
+        }
+        return x;
+    }
+
+    static int GenerateGeometric(double p)
+    {
+        double q = 1.0 - p, P = p, sum = P, z = rand.NextDouble();
+        int x = 0;
+        while (z > sum)
+        {
+            x++;
+            P *= q;
+            sum += P;
+        }
+        return x;
+    }
+
+    static int GenerateBinomial(int n, double p)
+    {
+        int x = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (rand.NextDouble() < p) x++;
+        }
+        return x;
+    }
+
+    public static void Run()
+    {
+        Console.Write("Enter lambda: ");
+        double lambda = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Poisson Distribution ({lambda})");
+        for (int i = 0; i < 200; i++) Console.Write(GeneratePoisson(lambda) + " ");
+        Console.WriteLine();
+
+        Console.Write("Enter p (for geometric): ");
+        double p_geom = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Geometric Distribution ({p_geom})");
+        for (int i = 0; i < 200; i++) Console.Write(GenerateGeometric(p_geom) + " ");
+        Console.WriteLine();
+
+        Console.Write("Enter n (for binomial): ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter p (for binomial): ");
+        double p_bin = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Binomial Distribution ({n}, {p_bin})");
+        for (int i = 0; i < 200; i++) Console.Write(GenerateBinomial(n, p_bin) + " ");
+        Console.WriteLine();
+    }
+}
+
